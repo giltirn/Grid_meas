8d071ed3bfe889051b7073b0ee8610a91b4d44d3
Christopher Kelly <ckelly@phys.columbia.edu>
2022-12-09 16:12:02 -0500

    Generalized split Grid function calls to accept arbitrary action Added split Grid routines for inverting on sources that can be rotated into matrices with X-conjugate columns + test

--- common/propagator_invert.h
+++ common/propagator_invert.h
@@ -162,13 +162,15 @@ namespace GridMeas{
   //subgrids_f must be the single precision subgrids
   //if do_midprop = true the msol_midprop output will be populated with the midpoint solutions
   template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
-  void splitGridMixedPrecInvertGen(std::vector<LatticeSCFmatrixD> &msol, std::vector<LatticeSCFmatrixD> &msol_midprop,
-				   const std::vector<LatticeSCFmatrixD> &msrc,
+  void splitGridMixedPrecInvertGen(std::vector<typename FermionActionD::PropagatorField> &msol, std::vector<typename FermionActionD::PropagatorField> &msol_midprop,
+				   const std::vector<typename FermionActionD::PropagatorField> &msrc,
 				   FermionActionD &action_d,
 				   FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 				   const MixedCGargs &args,
 				   bool do_midprop,
 				   std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
+    typedef typename FermionActionD::FermionField FermTypeD;
+    typedef typename FermionActionF::FermionField FermTypeF;
     int Nsrc = msrc.size();
     
     //Setup outputs
@@ -185,28 +187,30 @@ namespace GridMeas{
     std::cout << GridLogMessage << "Starting split Grid inversion with " << Nsubgrids << " subgrids" << std::endl;
     
     //Setup deflation
-    LinearFunction<FermionFieldD> *guesser = nullptr;
+    LinearFunction<FermTypeD> *guesser = nullptr;
     if(evecs != nullptr && evals != nullptr)
-      guesser = getGuesser<FermionFieldD>(*evals,*evecs);
+      guesser = getGuesser<FermTypeD>(*evals,*evecs);
 
     bool deflate = guesser != nullptr;
     
     //Setup the subgrid solver
-    SchurDiagMooeeOperator<FermionActionD,FermionFieldD> hermop_d(subgrid_action_d);
-    SchurDiagMooeeOperator<FermionActionF,FermionFieldF> hermop_f(subgrid_action_f);
+    SchurDiagMooeeOperator<FermionActionD,FermTypeD> hermop_d(subgrid_action_d);
+    SchurDiagMooeeOperator<FermionActionF,FermTypeF> hermop_f(subgrid_action_f);
 
-    LinearFunction<FermionFieldD>* mcg = MixedCGfactory(args, subgrid_action_f.FermionRedBlackGrid(),  hermop_f, hermop_d);
-    LinearFunctionWrapper<FermionFieldD> mcg_wrap(*mcg);  
-    SchurRedBlackDiagMooeeSolve<FermionFieldD> solver(mcg_wrap);
+    LinearFunction<FermTypeD>* mcg = MixedCGfactory(args, subgrid_action_f.FermionRedBlackGrid(),  hermop_f, hermop_d);
+    LinearFunctionWrapper<FermTypeD> mcg_wrap(*mcg);  
+    SchurRedBlackDiagMooeeSolve<FermTypeD> solver(mcg_wrap);
     
     //Setup scratch space for the subgrid solves
-    std::vector<FermionFieldD> split_sources_5d_full(Nsubgrids, action_d.FermionGrid());
-    std::vector<FermionFieldD> split_solutions_5d_full(Nsubgrids, action_d.FermionGrid());
-    FermionFieldD split_source_5d(subgrid_action_d.FermionGrid()); //the subgrid source
-    FermionFieldD split_solution_5d(subgrid_action_d.FermionGrid());
+    std::vector<FermTypeD> split_sources_5d_full(Nsubgrids, action_d.FermionGrid());
+    std::vector<FermTypeD> split_solutions_5d_full(Nsubgrids, action_d.FermionGrid());
+    FermTypeD split_source_5d(subgrid_action_d.FermionGrid()); //the subgrid source
+    FermTypeD split_solution_5d(subgrid_action_d.FermionGrid());
 
-    //Solve in blocks
-    int Ninvert = Ngp*Ns*Nc*Nsrc;
+    //Solve in block    
+    typedef columnOps<FermTypeD> cop;
+    int Ncols = cop::Ncols;
+    int Ninvert = Ncols*Nsrc;
     int Nblocks = (Ninvert + Nsubgrids -1)/Nsubgrids;
     std::cout << GridLogMessage << "Doing split Grid inversion of " << Ninvert << " source vectors in " << Nblocks << " blocks" << std::endl;
     for(int b=0;b<Nblocks;b++){
@@ -215,13 +219,10 @@ namespace GridMeas{
       int sidx=0; //split source/soln index
       std::cout << GridLogMessage << "Generating 5d sources for block " << b << std::endl;
       for(int i=istart;i<ilessthan;i++){ //i = c + Nc *( s + Ns*( f + Ngp * src ) ) 
-	int rem = i;
-	int c = rem % Nc; rem /= Nc;
-	int s = rem % Ns; rem /= Ns;
-	int f = rem % Ngp; rem /= Ngp;
-	int src = rem;
+	int pcol = i % Ncols;
+	int src = i / Ncols;
 
-	FermionFieldD src_4d = extractColumn(msrc[src], f,s,c);
+	FermTypeD src_4d = cop::extractColumn(msrc[src], pcol);
 	action_d.ImportPhysicalFermionSource(src_4d, split_sources_5d_full[sidx++]);
       }	
       while(sidx < Nsubgrids)
@@ -255,21 +256,18 @@ namespace GridMeas{
       Grid_unsplit(split_solutions_5d_full, split_solution_5d);
 
       std::cout << GridLogMessage << "Extracting 4d solutions" << std::endl;
-      FermionFieldD sol_4d(msrc[0].Grid());
+      FermTypeD sol_4d(msrc[0].Grid());
       sidx=0;
       for(int i=istart;i<ilessthan;i++){
-	int rem = i;
-	int c = rem % Nc; rem /= Nc;
-	int s = rem % Ns; rem /= Ns;
-	int f = rem % Ngp; rem /= Ngp;
-	int src = rem;
+	int pcol = i % Ncols;
+	int src = i / Ncols;
 	
 	action_d.ExportPhysicalFermionSolution(split_solutions_5d_full[sidx], sol_4d);
-	insertColumn(msol[src], sol_4d, f,s,c);
+	cop::insertColumn(msol[src], sol_4d, pcol);
 	
 	if(do_midprop){
 	  sol_4d = extractMidProp(split_solutions_5d_full[sidx], action_d);
-	  insertColumn(msol_midprop[src], sol_4d, f,s,c);	  
+	  cop::insertColumn(msol_midprop[src], sol_4d, pcol);	  
 	}
 	++sidx;
       }
@@ -280,57 +278,57 @@ namespace GridMeas{
 
   //Multi-src, no midprop
   template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
-  void splitGridMixedPrecInvert(std::vector<LatticeSCFmatrixD> &msol,
-				const std::vector<LatticeSCFmatrixD> &msrc,
+  void splitGridMixedPrecInvert(std::vector<typename FermionActionD::PropagatorField> &msol,
+				const std::vector<typename FermionActionD::PropagatorField> &msrc,
 				FermionActionD &action_d,
 				FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 				const MixedCGargs &args,
 				std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
-    std::vector<LatticeSCFmatrixD> tmp;
+    std::vector<typename FermionActionD::PropagatorField> tmp;
     splitGridMixedPrecInvertGen(msol, tmp, msrc, action_d, subgrid_action_d, subgrid_action_f, args, false, evals, evecs);
   }
   //Multi-src, no midprop, no evecs
   template<typename FermionActionD, typename FermionActionF>
-  void splitGridMixedPrecInvert(std::vector<LatticeSCFmatrixD> &msol,
-				const std::vector<LatticeSCFmatrixD> &msrc,
+  void splitGridMixedPrecInvert(std::vector<typename FermionActionD::PropagatorField> &msol,
+				const std::vector<typename FermionActionD::PropagatorField> &msrc,
 				FermionActionD &action_d,
 				FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 				const MixedCGargs &args){
-    splitGridMixedPrecInvert(msol, msrc, action_d, subgrid_action_d, subgrid_action_f, args, (std::vector<Real> const*)nullptr, (std::vector<FermionFieldD> const *)nullptr);
+    splitGridMixedPrecInvert(msol, msrc, action_d, subgrid_action_d, subgrid_action_f, args, (std::vector<Real> const*)nullptr, (std::vector<typename FermionActionD::FermionField> const *)nullptr);
   }
 
 
   //Single-src, no midprop
   template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
-  void splitGridMixedPrecInvert(LatticeSCFmatrixD &msol,
-				const LatticeSCFmatrixD &msrc,
+  void splitGridMixedPrecInvert(typename FermionActionD::PropagatorField &msol,
+				const typename FermionActionD::PropagatorField &msrc,
 				FermionActionD &action_d,
 				FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 				const MixedCGargs &args,
 				std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
     
-    std::vector<LatticeSCFmatrixD> tmp_msrc(1, msrc);
-    std::vector<LatticeSCFmatrixD> tmp_msol;
-    std::vector<LatticeSCFmatrixD> tmp_msol_mid;
+    std::vector<typename FermionActionD::PropagatorField> tmp_msrc(1, msrc);
+    std::vector<typename FermionActionD::PropagatorField> tmp_msol;
+    std::vector<typename FermionActionD::PropagatorField> tmp_msol_mid;
     splitGridMixedPrecInvertGen(tmp_msol, tmp_msol_mid, tmp_msrc, action_d, subgrid_action_d, subgrid_action_f, args, false, evals, evecs);
     msol = tmp_msol[0];
   }
   //Single-src, no midprop, no evecs
   template<typename FermionActionD, typename FermionActionF>
-  void splitGridMixedPrecInvert(LatticeSCFmatrixD &msol,
-				const LatticeSCFmatrixD &msrc,
+  void splitGridMixedPrecInvert(typename FermionActionD::PropagatorField &msol,
+				const typename FermionActionD::PropagatorField &msrc,
 				FermionActionD &action_d,
 				FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 				const MixedCGargs &args){
-    splitGridMixedPrecInvert(msol,msrc,action_d,subgrid_action_d,subgrid_action_f,args,(std::vector<Real> const*)nullptr, (std::vector<FermionFieldD> const *)nullptr);
+    splitGridMixedPrecInvert(msol,msrc,action_d,subgrid_action_d,subgrid_action_f,args,(std::vector<Real> const*)nullptr, (std::vector<typename FermionActionD::FermionField> const *)nullptr);
   }
 				
 
 
   //Multi-src with midprop
   template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
-  void splitGridMixedPrecInvertWithMidProp(std::vector<LatticeSCFmatrixD> &msol, std::vector<LatticeSCFmatrixD> &msol_mid,
-					   const std::vector<LatticeSCFmatrixD> &msrc,
+  void splitGridMixedPrecInvertWithMidProp(std::vector<typename FermionActionD::PropagatorField> &msol, std::vector<typename FermionActionD::PropagatorField> &msol_mid,
+					   const std::vector<typename FermionActionD::PropagatorField> &msrc,
 					   FermionActionD &action_d,
 					   FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 					   const MixedCGargs &args,
@@ -339,38 +337,38 @@ namespace GridMeas{
   }
   //Multi-src with midprop, no evecs
   template<typename FermionActionD, typename FermionActionF>
-  void splitGridMixedPrecInvertWithMidProp(std::vector<LatticeSCFmatrixD> &msol, std::vector<LatticeSCFmatrixD> &msol_mid,
-					   const std::vector<LatticeSCFmatrixD> &msrc,
+  void splitGridMixedPrecInvertWithMidProp(std::vector<typename FermionActionD::PropagatorField> &msol, std::vector<typename FermionActionD::PropagatorField> &msol_mid,
+					   const std::vector<typename FermionActionD::PropagatorField> &msrc,
 					   FermionActionD &action_d,
 					   FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 					   const MixedCGargs &args){
-    splitGridMixedPrecInvertWithMidProp(msol,msol_mid,msrc,action_d,subgrid_action_d,subgrid_action_f,args,(std::vector<Real> const*)nullptr, (std::vector<FermionFieldD> const *)nullptr);
+    splitGridMixedPrecInvertWithMidProp(msol,msol_mid,msrc,action_d,subgrid_action_d,subgrid_action_f,args,(std::vector<Real> const*)nullptr, (std::vector<typename FermionActionD::FermionField> const *)nullptr);
   }
 
 
   //Single-src with midprop
   template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
-  void splitGridMixedPrecInvertWithMidProp(LatticeSCFmatrixD &msol, LatticeSCFmatrixD &msol_mid,
-					   const LatticeSCFmatrixD &msrc,
+  void splitGridMixedPrecInvertWithMidProp(typename FermionActionD::PropagatorField &msol, typename FermionActionD::PropagatorField &msol_mid,
+					   const typename FermionActionD::PropagatorField &msrc,
 					   FermionActionD &action_d,
 					   FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 					   const MixedCGargs &args,
 					   std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
-    std::vector<LatticeSCFmatrixD> tmp_msrc(1, msrc);
-    std::vector<LatticeSCFmatrixD> tmp_msol;
-    std::vector<LatticeSCFmatrixD> tmp_msol_mid;        
+    std::vector<typename FermionActionD::PropagatorField> tmp_msrc(1, msrc);
+    std::vector<typename FermionActionD::PropagatorField> tmp_msol;
+    std::vector<typename FermionActionD::PropagatorField> tmp_msol_mid;        
     splitGridMixedPrecInvertGen(tmp_msol, tmp_msol_mid, tmp_msrc, action_d, subgrid_action_d, subgrid_action_f, args, true, evals, evecs);
     msol = tmp_msol[0];
     msol_mid = tmp_msol_mid[0];
   }
   //Single-src with midprop, no evecs
   template<typename FermionActionD, typename FermionActionF>
-  void splitGridMixedPrecInvertWithMidProp(LatticeSCFmatrixD &msol, LatticeSCFmatrixD &msol_mid,
-					   const LatticeSCFmatrixD &msrc,
+  void splitGridMixedPrecInvertWithMidProp(typename FermionActionD::PropagatorField &msol, typename FermionActionD::PropagatorField &msol_mid,
+					   const typename FermionActionD::PropagatorField &msrc,
 					   FermionActionD &action_d,
 					   FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
 					   const MixedCGargs &args){
-    splitGridMixedPrecInvertWithMidProp(msol,msol_mid,msrc,action_d,subgrid_action_d,subgrid_action_f,args, (std::vector<Real> const*)nullptr, (std::vector<FermionFieldD> const *)nullptr);
+    splitGridMixedPrecInvertWithMidProp(msol,msol_mid,msrc,action_d,subgrid_action_d,subgrid_action_f,args, (std::vector<Real> const*)nullptr, (std::vector<typename FermionActionD::FermionField> const *)nullptr);
   }
 
 
--- common/propagator_invert_Xconj.h
+++ common/propagator_invert_Xconj.h
@@ -161,7 +161,142 @@ namespace GridMeas{
   }
 
 
+  template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
+  void splitGridMixedPrecInvertGenXconj(std::vector<LatticeSCFmatrixD> &prop, std::vector<LatticeSCFmatrixD> &midprop, 
+					const std::vector<LatticeSCFmatrixD> &msrc, 
+					FermionActionD &xconj_action_d, 
+					FermionActionD &xconj_subgrid_action_d, FermionActionF &xconj_subgrid_action_f, 
+					const MixedCGargs &args,
+					bool do_midprop,
+					std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
+    int nsrc = msrc.size();
+    GridBase* grid = msrc[0].Grid();
+    LatticePropagatorD tmp(grid);
+    LatticeSCFmatrixD src_rotated(grid);
+    
+    int ninv = Ngp * nsrc; //mapping   f + Ngp * s
+    std::vector<LatticePropagatorD> src_fcol(ninv,grid);
 
+    for(int s=0;s<nsrc;s++){
+      src_rotated = mulHRight(msrc[s]); //the columns of this are X-conjugate
+
+      for(int fcol=0;fcol<Ngp;fcol++){
+	int idx = fcol + Ngp * s;
+	src_fcol[idx] = PeekIndex<GparityFlavourIndex>(src_rotated,0,fcol); //only need upper flavor component as X-conjugate      
+	tmp = PeekIndex<GparityFlavourIndex>(src_rotated,1,fcol) + Xmatrix()*conjugate(src_fcol[idx]);       //Check the columns are actually X-conjugate!
+	assert(norm2(tmp) < 1e-12);
+      }
+    }      
+
+    std::vector<LatticePropagatorD> prop_fcol(ninv, grid), midprop_fcol(do_midprop ? ninv : 0, grid);    
+    splitGridMixedPrecInvertGen(prop_fcol, midprop_fcol, src_fcol, xconj_action_d, xconj_subgrid_action_d, xconj_subgrid_action_f, args, do_midprop, evals, evecs);
+
+    for(int s=0;s<nsrc;s++){
+      get2fXconjMatrix(prop[s], prop_fcol[0+Ngp*s],prop_fcol[1+Ngp*s]);
+      prop[s] = mulHdagRight(prop[s]);
+
+      if(do_midprop){
+	get2fXconjMatrix(midprop[s], midprop_fcol[0+Ngp*s],midprop_fcol[1+Ngp*s]);
+	midprop[s] = mulHdagRight(midprop[s]);
+      }
+    }
+  }
+
+
+  //Multi-src, no midprop
+  template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
+  void splitGridMixedPrecInvertXconj(std::vector<LatticeSCFmatrixD> &msol,
+				     const std::vector<LatticeSCFmatrixD> &msrc,
+				     FermionActionD &action_d,
+				     FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
+				     const MixedCGargs &args,
+				     std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
+    std::vector<LatticeSCFmatrixD> tmp;
+    splitGridMixedPrecInvertGenXconj(msol, tmp, msrc, action_d, subgrid_action_d, subgrid_action_f, args, false, evals, evecs);
+  }
+  //Multi-src, no midprop, no evecs
+  template<typename FermionActionD, typename FermionActionF>
+  void splitGridMixedPrecInvertXconj(std::vector<LatticeSCFmatrixD> &msol,
+				     const std::vector<LatticeSCFmatrixD> &msrc,
+				     FermionActionD &action_d,
+				     FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
+				     const MixedCGargs &args){
+    splitGridMixedPrecInvertXconj(msol, msrc, action_d, subgrid_action_d, subgrid_action_f, args, (std::vector<Real> const*)nullptr, (std::vector<typename FermionActionD::FermionField> const *)nullptr);
+  }
+
+
+  //Single-src, no midprop
+  template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
+  void splitGridMixedPrecInvertXconj(LatticeSCFmatrixD &msol,
+				     const LatticeSCFmatrixD &msrc,
+				     FermionActionD &action_d,
+				     FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
+				     const MixedCGargs &args,
+				     std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
+    
+    std::vector<LatticeSCFmatrixD> tmp_msrc(1, msrc);
+    std::vector<LatticeSCFmatrixD> tmp_msol;
+    std::vector<LatticeSCFmatrixD> tmp_msol_mid;
+    splitGridMixedPrecInvertGenXconj(tmp_msol, tmp_msol_mid, tmp_msrc, action_d, subgrid_action_d, subgrid_action_f, args, false, evals, evecs);
+    msol = tmp_msol[0];
+  }
+  //Single-src, no midprop, no evecs
+  template<typename FermionActionD, typename FermionActionF>
+  void splitGridMixedPrecInvertXconj(LatticeSCFmatrixD &msol,
+				     const LatticeSCFmatrixD &msrc,
+				     FermionActionD &action_d,
+				     FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
+				     const MixedCGargs &args){
+    splitGridMixedPrecInvertXconj(msol,msrc,action_d,subgrid_action_d,subgrid_action_f,args,(std::vector<Real> const*)nullptr, (std::vector<typename FermionActionD::FermionField> const *)nullptr);
+  }
+				
+
+
+  //Multi-src with midprop
+  template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
+  void splitGridMixedPrecInvertWithMidPropXconj(std::vector<LatticeSCFmatrixD> &msol, std::vector<LatticeSCFmatrixD> &msol_mid,
+						const std::vector<LatticeSCFmatrixD> &msrc,
+						FermionActionD &action_d,
+						FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
+						const MixedCGargs &args,
+						std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
+    splitGridMixedPrecInvertGenXconj(msol, msol_mid, msrc, action_d, subgrid_action_d, subgrid_action_f, args, true, evals, evecs);
+  }
+  //Multi-src with midprop, no evecs
+  template<typename FermionActionD, typename FermionActionF>
+  void splitGridMixedPrecInvertWithMidPropXconj(std::vector<LatticeSCFmatrixD> &msol, std::vector<LatticeSCFmatrixD> &msol_mid,
+						const std::vector<LatticeSCFmatrixD> &msrc,
+						FermionActionD &action_d,
+						FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
+						const MixedCGargs &args){
+    splitGridMixedPrecInvertWithMidPropXconj(msol,msol_mid,msrc,action_d,subgrid_action_d,subgrid_action_f,args,(std::vector<Real> const*)nullptr, (std::vector<typename FermionActionD::FermionField> const *)nullptr);
+  }
+
+
+  //Single-src with midprop
+  template<typename FermionActionD, typename FermionActionF, typename EvecFieldType>
+  void splitGridMixedPrecInvertWithMidPropXconj(LatticeSCFmatrixD &msol, LatticeSCFmatrixD &msol_mid,
+						const LatticeSCFmatrixD &msrc,
+						FermionActionD &action_d,
+						FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
+						const MixedCGargs &args,
+						std::vector<Real> const* evals, std::vector<EvecFieldType> const * evecs){
+    std::vector<LatticeSCFmatrixD> tmp_msrc(1, msrc);
+    std::vector<LatticeSCFmatrixD> tmp_msol;
+    std::vector<LatticeSCFmatrixD> tmp_msol_mid;        
+    splitGridMixedPrecInvertGenXconj(tmp_msol, tmp_msol_mid, tmp_msrc, action_d, subgrid_action_d, subgrid_action_f, args, true, evals, evecs);
+    msol = tmp_msol[0];
+    msol_mid = tmp_msol_mid[0];
+  }
+  //Single-src with midprop, no evecs
+  template<typename FermionActionD, typename FermionActionF>
+  void splitGridMixedPrecInvertWithMidPropXconj(LatticeSCFmatrixD &msol, LatticeSCFmatrixD &msol_mid,
+						const LatticeSCFmatrixD &msrc,
+						FermionActionD &action_d,
+						FermionActionD &subgrid_action_d, FermionActionF &subgrid_action_f,
+						const MixedCGargs &args){
+    splitGridMixedPrecInvertWithMidPropXconj(msol,msol_mid,msrc,action_d,subgrid_action_d,subgrid_action_f,args, (std::vector<Real> const*)nullptr, (std::vector<typename FermionActionD::FermionField> const *)nullptr);
+  }
 
 
 };
